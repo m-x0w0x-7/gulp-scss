2a21a20a9308e28ca1f74b9fb970a6eb86490639
MiO <m.x0w0x.7@gmail.com>
2020-02-24 15:48:25 +0900

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,4 @@
 const gulp = require('gulp');
-const fs = require('fs');
-
-// pug
-const pug = require('gulp-pug');
-const data = require('gulp-data');
-
-// ejs
-const ejs = require('gulp-ejs');
-const rename = require('gulp-rename');
-const replace = require('gulp-replace');
 
 // scss
 const sass = require('gulp-sass');
@@ -17,74 +7,17 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const flexBugsFixes = require('postcss-flexbugs-fixes');
 
-// JS
-const webpackStream = require('webpack-stream');
-const webpack = require('webpack');
-
-// img
-const imagemin = require('gulp-imagemin');
-const mozjpeg = require('imagemin-mozjpeg');
-const pngquant = require('imagemin-pngquant');
-
 // utility
 const browserSync = require('browser-sync').create();
 const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
-const gulpif = require('gulp-if');
 const del = require('del');
 
 
 const paths = {
-  pug: './src/pug',
-  ejs: './src/ejs',
   scss: './src/scss',
-  javascript: './src/js',
-  image: './src/img',
   root: './dist',
-  css: './dist/css',
-  js: './dist/js',
-  img: './dist/img'
-}
-
-
-
-// pug
-function pugFunc() {
-  // JSONファイルの読み込みと変換
-  const configPugData = fs.readFileSync(paths.pug + '/data/config.json');
-  const configPugObj = JSON.parse(configPugData);
-
-  return gulp
-    .src([ paths.pug + '/**/*.pug', '!' + paths.pug + '/**/_*.pug' ])
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-    .pipe(data(file => {
-      return configPugObj;
-    }))
-    .pipe(pug({ pretty: true }))
-    .pipe(gulp.dest( paths.root ))
-    .pipe(browserSync.reload({ stream: true }));
-}
-
-
-// ejs
-function ejsFunc() {
-  // JSONファイルの読み込みと変換
-  const configJsonData = fs.readFileSync( paths.ejs + '/data/config.json' );
-  const configObj = JSON.parse( configJsonData );
-
-  // ejsのデータ読み込み設定
-  const ejsDataOption = {
-    config: configObj
-  };
-
-  return gulp
-    .src([ paths.ejs + '/**/*.ejs', '!' + paths.ejs + '/**/_*.ejs' ])
-    .pipe(plumber({ errorHandler: notify.onError('Error: <%= error.message %>') }))
-    .pipe(ejs( ejsDataOption, {}, {} ))
-    .pipe(rename({ extname: '.html' }))
-    .pipe(replace(/[\s\S]*?(<!DOCTYPE)/, '$1'))
-    .pipe(gulp.dest( paths.root ))
-    .pipe(browserSync.reload({ stream: true }));
+  css: './dist/css'
 }
 
 
@@ -135,42 +68,6 @@ function scssBuild() {
 }
 
 
-// JS
-// webpackの設定ファイルの読み込み
-function js() {
-  const webpackConfig = require('./webpack.config');
-
-  return webpackStream(webpackConfig, webpack)
-    .pipe(gulp.dest(paths.js))
-    .pipe(browserSync.reload({ stream: true }));
-}
-
-
-// image
-function image() {
-  const imageminOption = [
-    pngquant({
-      quality: [0.65, 0.8]
-    }),
-    mozjpeg({
-      quality: 80
-    }),
-    imagemin.gifsicle(),
-    imagemin.jpegtran(),
-    imagemin.optipng(),
-    imagemin.svgo({
-      removeViewBox: false
-    })
-  ];
-
-  return gulp
-  .src( paths.image + '/*' )
-  .pipe(imagemin( imageminOption ))
-  .pipe(gulp.dest( paths.img ))
-  .pipe(browserSync.reload({ stream: true }));
-}
-
-
 // browser sync
 function serve(done) {
   browserSync.init({
@@ -190,11 +87,7 @@ function serve(done) {
 
 // watch
 function watch(done) {
-  gulp.watch(paths.pug + '/**/*.pug', gulp.parallel(pugFunc));
-  gulp.watch(paths.ejs + '/**/*.ejs', gulp.parallel(ejsFunc));
   gulp.watch(paths.scss + '/**/*.scss', gulp.parallel(scss));
-  gulp.watch(paths.javascript + '/**/*.js', gulp.parallel(js));
-  gulp.watch(paths.image + '/**/*', gulp.parallel(image));
   done();
 }
 
@@ -207,13 +100,13 @@ function clean() {
 
 // default task(dev)
 exports.default = gulp.series(
-  gulp.parallel(pugFunc, ejsFunc, scss, js, image),
+  gulp.parallel(scss),
   gulp.parallel(serve, watch)
 );
 
 // default task(build)
 exports.build = gulp.series(
   clean,
-  gulp.parallel(pugFunc, ejsFunc, scssBuild, js, image),
+  gulp.parallel(scssBuild),
   gulp.parallel(serve, watch)
 )
